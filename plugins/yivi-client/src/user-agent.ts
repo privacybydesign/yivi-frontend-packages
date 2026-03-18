07902b86ec86e9fdf50b63fdb6520804febfc7ce
Ruben Hensen <ruben.hensen@protonmail.com>
2026-03-18 14:07:02 +0100

    Use declare global for Window/Document type extensions in user-agent
    Replace custom interfaces with global augmentation, allowing direct
    access to MSStream, MSInputMethodContext, and documentMode on the
    global window and document objects.

--- plugins/yivi-client/src/user-agent.ts
+++ plugins/yivi-client/src/user-agent.ts
@@ -1,30 +1,29 @@
 export type UserAgentType = 'nodejs' | 'Android' | 'iOS' | 'Desktop';
 
-interface WindowWithMSStream extends Window {
-  MSStream?: unknown;
-  MSInputMethodContext?: unknown;
-}
+declare global {
+  interface Window {
+    MSStream?: unknown;
+    MSInputMethodContext?: unknown;
+  }
 
-interface DocumentWithDocumentMode extends Document {
-  documentMode?: number;
+  interface Document {
+    documentMode?: number;
+  }
 }
 
 export default function userAgent(): UserAgentType {
   if (typeof window === 'undefined') return 'nodejs';
 
-  const win = window as WindowWithMSStream;
-  const doc = document as DocumentWithDocumentMode;
-
   // IE11 doesn't have window.navigator, test differently
   // https://stackoverflow.com/questions/21825157/internet-explorer-11-detection
-  if (!!win.MSInputMethodContext && !!doc.documentMode) return 'Desktop';
+  if (!!window.MSInputMethodContext && !!document.documentMode) return 'Desktop';
 
   if (/Android/i.test(window.navigator.userAgent)) {
     return 'Android';
   }
 
   // https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
-  if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !win.MSStream) return 'iOS';
+  if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) return 'iOS';
 
   // https://stackoverflow.com/questions/57776001/how-to-detect-ipad-pro-as-ipad-using-javascript
   if (/Macintosh/.test(navigator.userAgent) && navigator.maxTouchPoints && navigator.maxTouchPoints > 2) return 'iOS';
