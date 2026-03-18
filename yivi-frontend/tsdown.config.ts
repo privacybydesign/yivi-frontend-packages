576b6ac80252a93a25046edf95bfc10358774350
Ruben Hensen <ruben.hensen@protonmail.com>
2026-03-18 15:35:47 +0100

    Suppress MIXED_EXPORTS warnings in tsdown configs
    Add outputOptions: { exports: 'named' } to all tsdown configs
    to handle modules that mix default and named exports.

--- yivi-frontend/tsdown.config.ts
+++ yivi-frontend/tsdown.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   dts: true,
-
+  outputOptions: { exports: 'named' },
   sourcemap: true,
   clean: false, // Don't clean as we also have webpack output
   deps: {
